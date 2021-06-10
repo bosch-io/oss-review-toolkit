6de6fcd9d99b35ededc0e1ef97b789117f13ab1d
Oliver Heger <oliver.heger@bosch.io>
2021-06-10 16:01:40 +0200

    OrtResult: Use an interface to access the dependency information
    Currently, dependencies stored in an OrtResult are queried in terms of
    concrete representation classes like Scope or PackageReference. This
    is a problem when we want to switch completely to the dependency graph
    format because these representations are not used there.
    
    So, to prepare this move, introduce an interface DependencyNavigator
    that is going to be used in future to access information related to
    dependencies. Provide an implementation for this interface, which is
    based on the classes used by the classic dependency tree
    representation. Later on, an implementation using the dependency graph
    format will be added.
    
    The approach is to start with a minimum set of functions supporting a
    specific use case. Then functions to support other use cases can be
    added step by step.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

---
+++ model/src/main/kotlin/DependencyTreeNavigator.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2021 Bosch.IO GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.model
+
+/**
+ * A [DependencyNavigator] implementation based on the classic dependency tree format, which is represented by the
+ * [Project.scopes] property.
+ */
+object DependencyTreeNavigator : DependencyNavigator {
+    override fun scopeNames(project: Project): Set<String> = project.scopes.mapTo(mutableSetOf(), Scope::name)
+
+    override fun directDependencies(project: Project, scopeName: String): Sequence<DependencyNode> =
+        project.scopes.find { it.name == scopeName }?.dependencies?.asSequence().orEmpty()
+
+    override fun scopeDependencies(
+        project: Project,
+        maxDepth: Int,
+        matcher: DependencyMatcher
+    ): Map<String, Set<Identifier>> =
+        project.scopes.associate { scope ->
+            scope.name to scope.collectDependencies(maxDepth) { matcher(it) }
+        }
+}
